d883a7807dbca7762702e78beb1aa02ca89740a4
hoamx <hoamx.work@gmail.com>
2023-10-30 22:27:44 +0700

    feat: add validation form

--- src/authPages/LoginPage/LoginPage.js
+++ src/authPages/LoginPage/LoginPage.js
@@ -1,15 +1,22 @@
 import AuthBox from '../../shared/components/AuthBox';
+import { validateLoginForm } from '../../shared/utils/validators';
 import LoginPageFooter from './LoginPageFooter';
 import LoginPageHeader from './LoginPageHeader';
 import LoginPageInput from './LoginPageInput';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function LoginPage() {
   const [mail, setMail] = useState('');
   const [password, setPassword] = useState('');
   const [isFormValid, setIsFormValid] = useState(false);
 
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+
   const handleLogin = (event) => {
+    console.log(mail);
+    console.log(password);
     console.log('LOGIN');
   };
   return (
