4e0d0d0dd85a89d8dfe22a1d72811611c3594b82
hoamx <hoamx.work@gmail.com>
2023-10-30 22:09:08 +0700

    feat: add RedirectInfo component

--- src/authPages/LoginPage/LoginPageFooter.js
+++ src/authPages/LoginPage/LoginPageFooter.js
@@ -1,15 +1,31 @@
+import { Fragment } from 'react';
 import CustomPrimaryButton from '../../shared/components/CustomPrimaryButton';
+import RedirectInfo from '../../shared/components/RedirectInfo';
+import { useHistory } from 'react-router-dom';
 
 function LoginPageFooter({ handleLogin, isFormValid }) {
+  const history = useHistory();
+
+  const handlePushToRegisterPage = (event) => {
+    history.push('/register');
+  };
   return (
-    <div>
-      <CustomPrimaryButton
-        label="Login"
-        additionalStyles={{ marginTop: '30px' }}
-        disabled={!isFormValid}
-        onClick={handleLogin}
+    <Fragment>
+      <div>
+        <CustomPrimaryButton
+          label="Login"
+          additionalStyles={{ marginTop: '30px' }}
+          disabled={!isFormValid}
+          onClick={handleLogin}
+        />
+      </div>
+      <RedirectInfo
+        text="Need an account? "
+        redirectText="Create an account"
+        additionalStyles={{ marginTop: '5px' }}
+        redirectHandler={handlePushToRegisterPage}
       />
-    </div>
+    </Fragment>
   );
 }
 
