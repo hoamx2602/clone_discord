c494b29c532b66257050ffc766ff48d7252cea98
hoamx <hoamx.work@gmail.com>
2023-11-01 15:35:02 +0700

    feat: handle jwt token when logout

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -3,6 +3,10 @@ import SideBar from './SideBar/SideBar';
 import FriendsSideBar from './FriendsSideBar/FriendsSideBar';
 import Messenger from './Messenger/Messenger';
 import AppBar from './AppBar/AppBar';
+import { useEffect } from 'react';
+import { logout } from '../shared/utils/auth';
+import { connect } from 'react-redux';
+import { getActions } from '../store/actions/authAction';
 
 const Wrapper = styled('div')({
   width: '100%',
@@ -10,7 +14,16 @@ const Wrapper = styled('div')({
   display: 'flex',
 });
 
-function Dashboard() {
+function Dashboard({ setUserDetails }) {
+  useEffect(() => {
+    const userDetails = localStorage.getItem('user');
+
+    if (!userDetails) {
+      logout();
+    } else {
+      setUserDetails(JSON.parse(userDetails));
+    }
+  }, []);
   return (
     <Wrapper>
       <SideBar />
@@ -21,4 +34,10 @@ function Dashboard() {
   );
 }
 
-export default Dashboard;
+const mapActionsToProps = (dispatch) => {
+  return {
+    ...getActions(dispatch),
+  };
+};
+
+export default connect(null, mapActionsToProps)(Dashboard);
