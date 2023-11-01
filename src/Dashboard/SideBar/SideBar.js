eb65ee743e33221661ba7f7ea117443fefe26dad
hoamx <hoamx.work@gmail.com>
2023-11-01 10:13:00 +0700

    feat: add Group button

--- src/Dashboard/SideBar/SideBar.js
+++ src/Dashboard/SideBar/SideBar.js
@@ -1,15 +1,21 @@
 import { styled } from '@mui/system';
+import MainPageButton from './MainPageButton';
 
 const MainContainer = styled('div')({
   width: '72px',
   height: '100%',
   display: 'flex',
   flexDirection: 'column',
+  alignItems: 'center',
   backgroundColor: '#202225',
 });
 
 function SideBar() {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <MainPageButton />
+    </MainContainer>
+  );
 }
 
 export default SideBar;
