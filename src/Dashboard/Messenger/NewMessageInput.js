3cce5686a68060fbc90b50884e60310d3670f11d
hoa.mx <hoa.mx@kiotviet.com>
2023-11-07 07:52:34 +0700

    feat: handle send message

--- src/Dashboard/Messenger/NewMessageInput.js
+++ src/Dashboard/Messenger/NewMessageInput.js
@@ -1,5 +1,58 @@
-const NewMessageInput = () => {
-  return <div></div>;
+import { useState } from 'react';
+import { styled } from '@mui/system';
+import { connect } from 'react-redux';
+
+const MainContainer = styled('div')({
+  heigth: '60px',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const Input = styled('input')({
+  backgroundColor: '#2f3136',
+  height: '98%',
+  color: 'white',
+  border: 'none',
+  borderRadius: '8px',
+  fontSize: '14px',
+  padding: '0 10px',
+});
+
+const NewMessageInput = ({ chosenChatDetails }) => {
+  const [message, setMessage] = useState('');
+
+  const handleMessageValueChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleKeyPressed = (event) => {
+    if (event.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+
+  const handleSendMessage = () => {
+    console.log('DEBUG=================Sending message to server');
+    setMessage('');
+  };
+  return (
+    <MainContainer>
+      <Input
+        placeholder={`Write message to ${chosenChatDetails.name}`}
+        value={message}
+        onChange={handleMessageValueChange}
+        onKeyDown={handleKeyPressed}
+      />
+    </MainContainer>
+  );
 };
 
-export default NewMessageInput;
+const mapStoreStateToProps = ({ chat }) => {
+  return {
+    ...chat,
+  };
+};
+
+export default connect(mapStoreStateToProps)(NewMessageInput);
