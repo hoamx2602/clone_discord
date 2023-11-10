28fd5d5adb4d8bb2c52720d30b52435bd7b7c191
hoamx <hoamx.work@gmail.com>
2023-11-11 00:47:58 +0700

    feat: handle leave room

--- src/Dashboard/Room/RoomButtons/CloseRoomButton.js
+++ src/Dashboard/Room/RoomButtons/CloseRoomButton.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { IconButton } from '@mui/material';
 import { Close as CloseIcon } from '@mui/icons-material';
+import { leaveRoom } from '../../../realtimeCommunication/socketConnection';
 
 const CloseRoomButton = () => {
-  const handleLeaveRoom = () => {};
+  const handleLeaveRoom = () => {
+    leaveRoom();
+  };
   return (
     <IconButton onClick={handleLeaveRoom} style={{ color: 'white' }}>
       <CloseIcon />
