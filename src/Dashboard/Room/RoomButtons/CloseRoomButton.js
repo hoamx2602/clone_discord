e8c506e720bf38d478081d2cb745d014b18b012c
hoa.mx <hoa.mx@kiotviet.com>
2023-11-11 08:40:21 +0700

    fix: declare function leave room

--- src/Dashboard/Room/RoomButtons/CloseRoomButton.js
+++ src/Dashboard/Room/RoomButtons/CloseRoomButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { IconButton } from '@mui/material';
 import { Close as CloseIcon } from '@mui/icons-material';
-import { leaveRoom } from '../../../realtimeCommunication/socketConnection';
+import * as roomHandler from '../../../realtimeCommunication/roomHandler';
 
 const CloseRoomButton = () => {
   const handleLeaveRoom = () => {
-    leaveRoom();
+    roomHandler.leaveRoom();
   };
   return (
     <IconButton onClick={handleLeaveRoom} style={{ color: 'white' }}>
